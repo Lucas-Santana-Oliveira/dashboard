ca194b6c3b0057edf4b867ef73147c81e7651db3
l3c4s <lucassantanapgsp@gmail.com>
2024-02-27 01:34:40 -0300

    Initial commit

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'JavaScript Tutorial',
+        productNumber: '98334',
+        paymentStatus: 'Due',
+        status: 'Pending'
+    },
+    {
+        productName: 'Python Tutorial',
+        productNumber: '96875',
+        paymentStatus: 'Refunded',
+        status: 'Declined'
+    },
+    {
+        productName: 'Ruby Tutorial',
+        productNumber: '56453',
+        paymentStatus: 'Paid',
+        status: 'Active'
+    },
+]
